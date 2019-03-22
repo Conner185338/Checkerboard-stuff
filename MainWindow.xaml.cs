dbd7485f0df14405df89f25b7832e498c2543b45
Conner185338 <47399959+Conner185338@users.noreply.github.com>
2019-03-22 10:04:56 -0400

    Add files via upload
    files were uploaded

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _185338Checkers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 7; i > 0; i--)
+            {
+                    Ellipse w = new Ellipse();
+                    w.Height = i * 100;
+                    w.Width = i * 100;
+                    if (i % 2 == 0 )
+                    {
+                        w.Fill = Brushes.Red;
+                    }
+                    else
+                    {
+                        w.Fill = Brushes.Blue;
+                    }
+                    canvas.Children.Add(w);
+                    Canvas.SetLeft(w, 400 - (w.Width / 2));
+                    Canvas.SetTop(w, 400 - (w.Width / 2));
+            }
+        }
+    }
+}
